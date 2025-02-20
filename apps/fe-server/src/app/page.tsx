ed8a633b2cee9d528eb64a5c07572b4b69336046
suyashshri <suyashshrivastava736@gmail.com>
2025-02-20 22:52:23 +0530

    prettier

--- apps/fe-server/src/app/page.tsx
+++ apps/fe-server/src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <div className="text-3xl font-bold underline">Hello World</div>;
+  return <div className="text-3xl font-bold underline">
+    Hello World</div>
+  ;
 }
